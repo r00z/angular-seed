d83afa35ca6df1a78fe2bf041be8fadaea9fc0bf
iruzanov <igor.ruzanov@dsquare.be>
2013-11-22 15:05:09 +0100

    added shared service

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'd3']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
---
+++ test/unit/d3Spec.js
@@ -0,0 +1,8 @@
+'use strict';
+
+describe('controllers', function () {
+    beforeEach(module('d3'));
+
+    describe('d3', function() {
+    });
+});
